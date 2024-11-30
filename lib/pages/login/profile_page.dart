84ebbe8507246d36e53aa670478e2c81af1e9156
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-11-30 14:17:16 +0100

    Implement profile page

---
+++ lib/pages/login/profile_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Zalogowano jako:'),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Wyloguj się',
+                  style: TextStyle(color: Color.fromARGB(255, 73, 219, 235))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
