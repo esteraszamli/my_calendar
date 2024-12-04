6f2d40d361ec3127e7fdc9c5dc03604d67db8103
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-12-04 21:22:40 +0100

    Create note repository with data management operations

--- lib/pages/add_note/cubit/add_note_cubit.dart
+++ lib/pages/add_note/cubit/add_note_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_calendar/model/note_model.dart';
+import 'package:my_calendar/repository/notes_repository.dart';
 
 part 'add_note_state.dart';
 
