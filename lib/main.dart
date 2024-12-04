864bdb4872674bfdab8891fd6f7f7e0508d70681
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-12-04 16:55:21 +0100

    Add firebase auth to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:my_calendar/pages/calendar/calendar_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:my_calendar/pages/login/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -32,7 +34,25 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 192, 192, 192)),
         useMaterial3: true,
       ),
-      home: const CalendarPage(),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+        final user = snapshot.data;
+        return user == null ? LoginPage() : const CalendarPage();
+      },
     );
   }
 }
--- lib/pages/login/profile_page.dart
+++ lib/pages/login/profile_page.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Zalogowano jako:', style: TextStyle(fontSize: 16)),
+            Text('Zalogowano jako: ${user?.email ?? 'Brak emaila'}',
+                style: const TextStyle(fontSize: 16)),
             const SizedBox(height: 16),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
               child: const Text('Wyloguj się',
                   style: TextStyle(
                       color: Color.fromARGB(255, 39, 206, 225), fontSize: 16)),
