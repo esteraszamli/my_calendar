96ec258c4b1afea78086a612c4b56aad093aed32
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-12-05 14:14:56 +0100

    Implement bloc in add note page

--- lib/pages/add_note/add_note_page.dart
+++ lib/pages/add_note/add_note_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:my_calendar/injection_container.dart';
+import 'package:my_calendar/pages/add_note/cubit/add_note_cubit.dart';
 
 class AddNotePage extends StatefulWidget {
   final DateTime selectedDate;
@@ -29,59 +32,83 @@ class AddNotePageState extends State<AddNotePage> {
             ),
           ),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: _titleController,
-                  decoration: InputDecoration(
-                    hintText: 'Tytuł...',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+        body: BlocProvider<AddNoteCubit>(
+            create: (context) => getIt<AddNoteCubit>(),
+            child: BlocConsumer<AddNoteCubit, AddNoteState>(
+              listener: (context, state) {
+                if (state.noteAdded == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Notatka została zapisana')),
+                  );
+                  Navigator.pop(context);
+                } else if (state.errorMessage != null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content: Text('Błąd zapisu: ${state.errorMessage}')),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextField(
+                          controller: _titleController,
+                          decoration: InputDecoration(
+                            hintText: 'Tytuł...',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          maxLines: 1,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(35),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        TextField(
+                          controller: _contentController,
+                          decoration: InputDecoration(
+                            hintText: 'Treść notatki...',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          maxLines: 20,
+                        ),
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 16.0),
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: ElevatedButton(
+                              onPressed: state.isLoading
+                                  ? null
+                                  : () {
+                                      context.read<AddNoteCubit>().addNote();
+                                    },
+                              child: state.isLoading
+                                  ? const CircularProgressIndicator()
+                                  : const Text(
+                                      'Zapisz',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color:
+                                            Color.fromARGB(255, 39, 206, 225),
+                                      ),
+                                    ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  maxLines: 1,
-                  inputFormatters: [
-                    LengthLimitingTextInputFormatter(35),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: _contentController,
-                  decoration: InputDecoration(
-                    hintText: 'Treść notatki...',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  maxLines: 20,
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.only(right: 16.0),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Tu będzie implementacja zapisu notatki
-                        ('Notatka: ${_titleController.text}');
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Zapisz',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color.fromARGB(255, 39, 206, 225),
-                          )),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ));
+                );
+              },
+            )));
   }
 
   @override
