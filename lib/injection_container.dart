002706df9cc7b84d2ce4dc888a17def5de99d0ef
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-12-10 17:03:21 +0100

    Add note model to injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,7 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:my_calendar/data_source/notes_remote_data_source.dart';
-import 'package:my_calendar/model/note_model.dart';
+import 'package:my_calendar/models/holiday_model.dart';
+import 'package:my_calendar/models/note_model.dart';
 import 'package:my_calendar/pages/add_note/cubit/add_note_cubit.dart';
 import 'package:my_calendar/pages/calendar/cubit/calendar_cubit.dart';
 import 'package:my_calendar/pages/edit_note/cubit/edit_note_cubit.dart';
@@ -23,14 +24,26 @@ void configureDependencies() {
   getIt.registerFactory(() => NotesRepository(getIt<NotesRemoteDataSource>()));
 
   // Bloc
-  getIt.registerFactory(() =>
-      AddNoteCubit(getIt<NotesRepository>(), initialDate: DateTime.now()));
+  getIt.registerFactoryParam<AddNoteCubit, DateTime, void>(
+    (selectedDate, _) =>
+        AddNoteCubit(getIt<NotesRepository>(), initialDate: selectedDate),
+  );
   getIt.registerFactoryParam<EditNoteCubit, NoteModel, void>(
-      (note, _) => EditNoteCubit(getIt<NotesRepository>(), note));
+    (note, _) => EditNoteCubit(getIt<NotesRepository>(), note),
+  );
   getIt.registerFactory(() => CalendarCubit(getIt<NotesRepository>()));
   getIt.registerFactory(() => NoteCubit(getIt<NotesRepository>()));
 
-  // Model
+  // Models
   getIt.registerFactory(() => NoteModel(
-      id: '', title: '', content: '', dateTime: DateTime.now(), userID: ''));
+      id: '',
+      title: '',
+      content: '',
+      dateTime: DateTime.now(),
+      userID: '')); // Datę poprawić ???
+  getIt.registerFactory(() => HolidayModel(
+        localName: '',
+        countryCode: '',
+        date: '',
+      ));
 }
