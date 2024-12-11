70a0d677e6e70b7a0e8fdd0c853ff46450b8f06e
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-12-11 13:05:00 +0100

    Implement new data sources in GetIt

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:my_calendar/data_source/holiday_remote_data_source.dart';
 import 'package:my_calendar/data_source/notes_remote_data_source.dart';
 import 'package:my_calendar/models/holiday_model.dart';
 import 'package:my_calendar/models/note_model.dart';
@@ -8,6 +10,7 @@ import 'package:my_calendar/pages/add_note/cubit/add_note_cubit.dart';
 import 'package:my_calendar/pages/calendar/cubit/calendar_cubit.dart';
 import 'package:my_calendar/pages/edit_note/cubit/edit_note_cubit.dart';
 import 'package:my_calendar/pages/note/cubit/note_cubit.dart';
+import 'package:my_calendar/repository/holiday_repository.dart';
 import 'package:my_calendar/repository/notes_repository.dart';
 
 final getIt = GetIt.instance;
@@ -19,9 +22,21 @@ void configureDependencies() {
 
   // Data source
   getIt.registerFactory(() => NotesRemoteDataSource(getIt()));
+  getIt.registerFactory(() => HolidayRemoteDataSource(getIt()));
+
+  // Rejestracja Dio
+  getIt.registerLazySingleton<Dio>(() {
+    final dio = Dio();
+    // Możesz tu dodać dodatkową konfigurację Dio, np.:
+    dio.options.baseUrl = 'https://date.nager.at';
+    dio.options.connectTimeout = const Duration(seconds: 5);
+    dio.options.receiveTimeout = const Duration(seconds: 3);
+    return dio;
+  });
 
   // Repository
   getIt.registerFactory(() => NotesRepository(getIt<NotesRemoteDataSource>()));
+  getIt.registerFactory(() => HolidayRepository(getIt()));
 
   // Bloc
   getIt.registerFactoryParam<AddNoteCubit, DateTime, void>(
@@ -31,7 +46,8 @@ void configureDependencies() {
   getIt.registerFactoryParam<EditNoteCubit, NoteModel, void>(
     (note, _) => EditNoteCubit(getIt<NotesRepository>(), note),
   );
-  getIt.registerFactory(() => CalendarCubit(getIt<NotesRepository>()));
+  getIt.registerFactory(() =>
+      CalendarCubit(getIt<NotesRepository>(), getIt<HolidayRepository>()));
   getIt.registerFactory(() => NoteCubit(getIt<NotesRepository>()));
 
   // Models
@@ -40,7 +56,7 @@ void configureDependencies() {
       title: '',
       content: '',
       dateTime: DateTime.now(),
-      userID: '')); // Datę poprawić ???
+      userID: '')); // Obsługę daty poprawić ?
   getIt.registerFactory(() => HolidayModel(
         localName: '',
         countryCode: '',
