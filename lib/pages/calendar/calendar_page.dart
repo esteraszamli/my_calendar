e32d454f5014a6950dbb0a4dbbe567045cc38bce
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-12-12 19:46:16 +0100

    Refactor CalendarPage: Improve scrolling and fix style of UI

--- lib/pages/calendar/calendar_page.dart
+++ lib/pages/calendar/calendar_page.dart
@@ -28,7 +28,7 @@ class _CalendarPageState extends State<CalendarPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Color.fromARGB(255, 99, 222, 231),
         title: Text(
           'Mój Kalendarz',
           style: GoogleFonts.outfit(fontSize: 23, fontWeight: FontWeight.w400),
@@ -51,7 +51,7 @@ class _CalendarPageState extends State<CalendarPage> {
             label: 'Moje konto',
           ),
         ],
-        selectedItemColor: const Color.fromARGB(255, 104, 227, 243),
+        selectedItemColor: const Color.fromARGB(255, 39, 206, 225),
       ),
       body: BlocProvider<CalendarCubit>(
         create: (context) => getIt<CalendarCubit>()..start(DateTime.now()),
@@ -70,140 +70,160 @@ class _CalendarPageState extends State<CalendarPage> {
             if (currentIndex == 1) {
               return const ProfilePage();
             }
-            return NestedScrollView(
-              headerSliverBuilder:
-                  (BuildContext context, bool innerBoxIsScrolled) {
-                return <Widget>[
-                  SliverToBoxAdapter(
-                    child: TableCalendar(
-                      locale: 'pl',
-                      firstDay: DateTime(2022),
-                      lastDay: DateTime(2030),
-                      focusedDay: _focusedDay,
-                      calendarFormat: _calendarFormat,
-                      headerStyle: const HeaderStyle(
-                        formatButtonVisible: false,
-                      ),
-                      selectedDayPredicate: (day) {
-                        return isSameDay(_selectedDay, day);
-                      },
-                      calendarStyle: CalendarStyle(
-                        todayDecoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.inversePrimary,
-                          shape: BoxShape.circle,
-                        ),
-                        selectedDecoration: const BoxDecoration(
-                          color: Color.fromARGB(255, 167, 238, 246),
-                          shape: BoxShape.circle,
-                        ),
-                        defaultDecoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        holidayDecoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 0, 64, 193)
-                              .withOpacity(0.2),
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                      holidayPredicate: (day) => _isHoliday(day, state),
-                      onDaySelected: (selectedDay, focusedDay) {
-                        if (!isSameDay(_selectedDay, selectedDay)) {
-                          setState(() {
-                            _selectedDay = selectedDay;
-                            _focusedDay = focusedDay;
-                            _dateTapCount = 1;
-                            context.read<CalendarCubit>().start(selectedDay);
-                          });
-                        } else {
-                          setState(() {
-                            _dateTapCount++;
-                          });
-                          if (_dateTapCount == 2) {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  actionsAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  title: const Text(
-                                    'Stwórz notatkę',
-                                    textAlign: TextAlign.center,
-                                  ),
-                                  content: const Text(
-                                    'Czy chcesz utworzyć nową notatkę?',
-                                    style: TextStyle(fontSize: 16),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      child: const Text(
-                                        'Anuluj',
-                                        style: TextStyle(fontSize: 16),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                    TextButton(
-                                      child: const Text(
-                                        'Ok',
-                                        style: TextStyle(fontSize: 16),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => AddNotePage(
-                                                selectedDate: selectedDay),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          }
-                        }
-                      },
-                      onFormatChanged: (format) {
-                        setState(() {
-                          _calendarFormat = format;
-                        });
-                      },
-                      onPageChanged: (focusedDay) {
-                        setState(() {
-                          _focusedDay = focusedDay;
-                        });
-                      },
-                    ),
-                  ),
-                  const SliverToBoxAdapter(
-                    child: Divider(
-                      color: Colors.grey,
-                      thickness: 1,
-                      indent: 16,
-                      endIndent: 16,
-                    ),
-                  ),
-                ];
-              },
-              body: Column(
-                children: [
-                  // Dodanie informacji o święcie
-                  _buildHolidayInfo(state),
-                  Expanded(
-                    child: _selectedDay != null
-                        ? _buildNotesList(_selectedDay!)
-                        : _buildNotesList(DateTime.now()),
-                  ),
-                ],
-              ),
-            );
+            return _buildCalendarContent(context, state);
           },
         ),
       ),
+    );
+  }
+
+  Widget _buildCalendarContent(BuildContext context, CalendarState state) {
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: TableCalendar(
+            locale: 'pl',
+            firstDay: DateTime(2023),
+            lastDay: DateTime(2030),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: GoogleFonts.outfit(
+                fontSize: 17,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            daysOfWeekStyle: DaysOfWeekStyle(
+              weekdayStyle: TextStyle(
+                fontFamily: GoogleFonts.outfit().fontFamily,
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+              ),
+              weekendStyle: TextStyle(
+                fontFamily: GoogleFonts.outfit().fontFamily,
+                fontWeight: FontWeight.w400,
+                color: const Color.fromARGB(255, 121, 121, 121),
+                fontSize: 14,
+              ),
+            ),
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            calendarStyle: CalendarStyle(
+              todayDecoration: BoxDecoration(
+                color: Color.fromARGB(255, 99, 222, 231).withOpacity(0.7),
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Color.fromARGB(255, 99, 222, 231).withOpacity(0.5),
+                shape: BoxShape.circle,
+              ),
+              defaultDecoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              holidayDecoration: BoxDecoration(
+                color: Color.fromARGB(255, 49, 174, 191).withOpacity(0.6),
+                shape: BoxShape.circle,
+              ),
+            ),
+            holidayPredicate: (day) => _isHoliday(day, state),
+            onDaySelected: (selectedDay, focusedDay) {
+              _handleDaySelection(context, selectedDay, focusedDay);
+            },
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            onPageChanged: (focusedDay) {
+              setState(() {
+                _focusedDay = focusedDay;
+              });
+            },
+          ),
+        ),
+        const SliverToBoxAdapter(
+          child: Divider(
+            color: Colors.grey,
+            thickness: 1,
+            indent: 16,
+            endIndent: 16,
+          ),
+        ),
+        if (_selectedDay != null)
+          SliverToBoxAdapter(
+            child: _buildHolidayInfo(state),
+          ),
+        _buildNotesListSliver(state),
+      ],
+    );
+  }
+
+  void _handleDaySelection(
+      BuildContext context, DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _dateTapCount = 1;
+        context.read<CalendarCubit>().start(selectedDay);
+      });
+    } else {
+      setState(() {
+        _dateTapCount++;
+      });
+      if (_dateTapCount % 2 == 0) {
+        _showAddNoteDialog(context, selectedDay);
+      }
+    }
+  }
+
+  void _showAddNoteDialog(BuildContext context, DateTime selectedDay) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          actionsAlignment: MainAxisAlignment.spaceAround,
+          title: const Text(
+            'Stwórz notatkę',
+            textAlign: TextAlign.center,
+          ),
+          content: const Text(
+            'Czy chcesz utworzyć nową notatkę?',
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Anuluj',
+                style: TextStyle(fontSize: 16),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text(
+                'Ok',
+                style: TextStyle(fontSize: 16),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        AddNotePage(selectedDate: selectedDay),
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -218,10 +238,11 @@ class _CalendarPageState extends State<CalendarPage> {
                   const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
               child: Text(
                 'Święto: $holidayName',
-                style: const TextStyle(
-                  fontSize: 16,
+                style: GoogleFonts.outfit(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
                   fontStyle: FontStyle.italic,
-                  color: Color.fromARGB(255, 0, 162, 183),
+                  color: Color.fromARGB(255, 49, 174, 191),
                 ),
               ),
             ),
@@ -238,7 +259,91 @@ class _CalendarPageState extends State<CalendarPage> {
     return const SizedBox.shrink();
   }
 
-  // Metody do obsługi świąt
+  Widget _buildNotesListSliver(CalendarState state) {
+    final notes = state.notes;
+    return notes.isNotEmpty
+        ? SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final note = notes[index];
+                return Column(
+                  children: [
+                    ListTile(
+                      title: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              note.title,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: GoogleFonts.outfit(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w600,
+                                color: Color.fromARGB(255, 49, 174, 191),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            note.wasCreatedWithTime
+                                ? DateFormat('HH:mm').format(note.dateTime)
+                                : '',
+                            style: GoogleFonts.outfit(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        ],
+                      ),
+                      subtitle: Text(
+                        note.content,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: GoogleFonts.outfit(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      onTap: () async {
+                        final cubit = context.read<CalendarCubit>();
+                        final wasModified = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => NotePage(
+                              noteID: note.id,
+                              noteDate: note.dateTime,
+                            ),
+                          ),
+                        );
+                        if (wasModified == true) {
+                          cubit.start();
+                        }
+                      },
+                    ),
+                    if (index < notes.length - 1)
+                      const Divider(
+                        height: 1,
+                        indent: 16,
+                        endIndent: 16,
+                      ),
+                  ],
+                );
+              },
+              childCount: notes.length,
+            ),
+          )
+        : SliverFillRemaining(
+            child: Center(
+              child: Text(
+                'Brak notatek',
+                style: GoogleFonts.outfit(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          );
+  }
+
   bool _isHoliday(DateTime day, CalendarState state) {
     return state.holidays.any((holiday) {
       final holidayDate = DateTime.parse(holiday.date);
@@ -249,81 +354,11 @@ class _CalendarPageState extends State<CalendarPage> {
   String? _getHolidayName(DateTime day, CalendarState state) {
     try {
       final holiday = state.holidays.firstWhere(
-          (holiday) => isSameDay(DateTime.parse(holiday.date), day));
+        (holiday) => isSameDay(DateTime.parse(holiday.date), day),
+      );
       return holiday.localName;
     } catch (e) {
       return null;
     }
-  }
-
-  // Budowanie listy notatek
-  Widget _buildNotesList(DateTime selectedDay) {
-    return BlocBuilder<CalendarCubit, CalendarState>(
-      builder: (context, state) {
-        final notes = state.notes;
-
-        return notes.isNotEmpty
-            ? ListView.separated(
-                itemCount: notes.length,
-                separatorBuilder: (context, index) => const Divider(
-                  height: 1,
-                  indent: 16,
-                  endIndent: 16,
-                ),
-                itemBuilder: (context, index) {
-                  final note = notes[index];
-                  return ListTile(
-                    title: Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            note.title,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17,
-                              color: Color.fromARGB(255, 49, 174, 191),
-                            ),
-                          ),
-                        ),
-                        Text(
-                          DateFormat('HH:mm').format(note.dateTime),
-                          style: TextStyle(
-                            color: Colors.black54,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                    subtitle: Text(
-                      note.content,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(fontSize: 15),
-                    ),
-                    onTap: () async {
-                      final cubit = context.read<CalendarCubit>();
-                      final wasModified = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => NotePage(
-                            noteID: note.id,
-                            noteDate: note.dateTime,
-                          ),
-                        ),
-                      );
-                      if (wasModified == true) {
-                        cubit.start();
-                      }
-                    },
-                  );
-                },
-              )
-            : const Center(
-                child: Text('Brak notatek', style: TextStyle(fontSize: 16)),
-              );
-      },
-    );
   }
 }
