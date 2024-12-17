c7da6f1c3f8716125b1affd90185c67c3951e272
Estera Szamli-Jonkisz <esthere.szamli@gmail.com>
2024-12-17 19:16:15 +0100

    Add user friendly error handling and hide unnecessary elements during password change

--- lib/pages/login/profile_page.dart
+++ lib/pages/login/profile_page.dart
@@ -12,34 +12,48 @@ class ProfilePage extends StatefulWidget {
 class ProfilePageState extends State<ProfilePage> {
   final _newPasswordController = TextEditingController();
   final _currentPasswordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
   bool _isPasswordChangeVisible = false;
+  String errorMessage = '';
+
+  String _getErrorMessage(FirebaseAuthException error) {
+    switch (error.code) {
+      case 'wrong-password':
+        return 'Nieprawidłowe aktualne hasło';
+      case 'weak-password':
+        return 'Hasło jest za słabe. Powinno mieć min. 6 znaków';
+      default:
+        return 'Wystąpił błąd zmiany hasła';
+    }
+  }
 
   Future<void> _changePassword() async {
-    if (_newPasswordController.text.length < 6) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-            content: Text(
-          'Hasło musi mieć minimum 6 znaków',
-          style: GoogleFonts.outfit(
-            fontSize: 15,
-            fontWeight: FontWeight.w400,
-          ),
-        )),
-      );
+    setState(() {
+      errorMessage = '';
+    });
+    if (_currentPasswordController.text.isEmpty ||
+        _newPasswordController.text.isEmpty) {
+      setState(() {
+        errorMessage = 'Uzupełnij wszystkie pola';
+      });
       return;
     }
-
+    if (_newPasswordController.text != _confirmPasswordController.text) {
+      setState(() {
+        errorMessage = 'Nowe hasła nie są identyczne';
+      });
+      return;
+    }
+    if (_newPasswordController.text.length < 6) {
+      setState(() {
+        errorMessage = 'Nowe hasło musi mieć minimum 6 znaków';
+      });
+      return;
+    }
     if (_newPasswordController.text.length > 30) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-            content: Text(
-          'Hasło może mieć maksymalnie 30 znaków',
-          style: GoogleFonts.outfit(
-            fontSize: 15,
-            fontWeight: FontWeight.w400,
-          ),
-        )),
-      );
+      setState(() {
+        errorMessage = 'Nowe hasło może mieć maksymalnie 30 znaków';
+      });
       return;
     }
     try {
@@ -49,96 +63,97 @@ class ProfilePageState extends State<ProfilePage> {
         email: user?.email ?? '',
         password: _currentPasswordController.text,
       );
-
       await user?.reauthenticateWithCredential(credential);
-
       await user?.updatePassword(_newPasswordController.text);
-
       _newPasswordController.clear();
       _currentPasswordController.clear();
+      _confirmPasswordController.clear();
       setState(() {
         _isPasswordChangeVisible = false;
       });
-
       if (mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-              content: Text(
-            'Hasło zostało zmienione',
-            style: GoogleFonts.outfit(
-              fontSize: 15,
-              fontWeight: FontWeight.w400,
+            content: Text(
+              'Hasło zostało zmienione',
+              style: GoogleFonts.outfit(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+              ),
             ),
-          )),
+          ),
         );
       }
     } on FirebaseAuthException catch (e) {
-      if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-              content: Text(
-            'Błąd: ${e.message}',
-            style: GoogleFonts.outfit(
-              fontSize: 15,
-              fontWeight: FontWeight.w400,
-            ),
-          )),
-        );
-      }
+      setState(() {
+        errorMessage = _getErrorMessage(e);
+      });
+    } catch (error) {
+      setState(() {
+        errorMessage = 'Wystąpił nieznany błąd: ${error.toString()}';
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
-
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Zalogowano jako: ${user?.email ?? 'Brak emaila'}',
-              style: GoogleFonts.outfit(
-                fontSize: 17,
-                fontWeight: FontWeight.w400,
+            if (!_isPasswordChangeVisible) ...[
+              Icon(
+                Icons.person,
+                size: 60,
+                color: Color.fromARGB(255, 99, 222, 231).withOpacity(0.8),
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-              },
-              child: Text(
-                'Wyloguj się',
+              const SizedBox(height: 25),
+              Text(
+                'Zalogowano jako: ${user?.email ?? 'Brak emaila'}',
                 style: GoogleFonts.outfit(
                   fontSize: 17,
-                  color: Color.fromARGB(255, 39, 206, 225),
+                  fontWeight: FontWeight.w400,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 25),
+              ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                },
+                child: Text(
+                  'Wyloguj się',
+                  style: GoogleFonts.outfit(
+                    fontSize: 17,
+                    color: Color.fromARGB(255, 39, 206, 225),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isPasswordChangeVisible = !_isPasswordChangeVisible;
+                });
+              },
+              child: Text(
+                _isPasswordChangeVisible
+                    ? 'Anuluj zmianę hasła'
+                    : 'Zmień hasło',
+                style: GoogleFonts.outfit(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 37, 151, 164),
                   fontWeight: FontWeight.w500,
                 ),
               ),
             ),
-            const SizedBox(height: 10),
-            TextButton(
-                onPressed: () {
-                  setState(() {
-                    _isPasswordChangeVisible = !_isPasswordChangeVisible;
-                  });
-                },
-                child: Text(
-                  _isPasswordChangeVisible
-                      ? 'Anuluj zmianę hasła'
-                      : 'Zmień hasło',
-                  style: GoogleFonts.outfit(
-                    fontSize: 16,
-                    color: Color.fromARGB(255, 37, 151, 164),
-                    fontWeight: FontWeight.w500,
-                  ),
-                )),
             if (_isPasswordChangeVisible) ...[
               const SizedBox(height: 10),
               SizedBox(
-                // ??? czcionka nie działa
                 width: 300,
                 child: TextField(
                   controller: _currentPasswordController,
@@ -170,16 +185,43 @@ class ProfilePageState extends State<ProfilePage> {
                 ),
               ),
               const SizedBox(height: 16),
-              ElevatedButton(
-                  onPressed: _changePassword,
-                  child: Text(
-                    'Potwierdź zmianę hasła',
-                    style: GoogleFonts.outfit(
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  style: GoogleFonts.outfit(
+                      fontSize: 15, fontWeight: FontWeight.w500),
+                  controller: _confirmPasswordController,
+                  decoration: InputDecoration(
+                    labelText: 'Potwierdź nowe hasło',
+                    labelStyle: GoogleFonts.outfit(
                       fontSize: 16,
-                      color: Color.fromARGB(255, 37, 151, 164),
-                      fontWeight: FontWeight.w500,
+                      fontWeight: FontWeight.w400,
                     ),
-                  )),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _changePassword,
+                child: Text(
+                  'Potwierdź zmianę hasła',
+                  style: GoogleFonts.outfit(
+                    fontSize: 16,
+                    color: Color.fromARGB(255, 37, 151, 164),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                child: Text(
+                  errorMessage,
+                  style: const TextStyle(color: Colors.red, fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
           ],
         ),
@@ -191,6 +233,7 @@ class ProfilePageState extends State<ProfilePage> {
   void dispose() {
     _newPasswordController.dispose();
     _currentPasswordController.dispose();
+    _confirmPasswordController.dispose();
     super.dispose();
   }
 }
