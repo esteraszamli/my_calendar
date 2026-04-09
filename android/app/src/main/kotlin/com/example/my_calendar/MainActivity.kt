5022597cf0c46cf3416263d0df2fc1e1a647c491
esteraszamli <esthere.szamli@gmail.com>
2026-04-09 11:01:20 +0200

    Update dependencies. Part two

--- android/app/src/main/kotlin/com/example/my_calendar/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_calendar/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.esteraszamlijonkisz.my_calendar
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.view.WindowCompat
+import android.os.Bundle
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+    }
+}
